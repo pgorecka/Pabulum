10416cd3737cc6c4a4b077ca170eeabf04892e0c
pgorecka <pgorecka@zoho.com>
2023-01-07 18:44:25 +0100

    implemented webViewClient to display recipe instructions directly from website

--- app/src/main/java/com/example/pabulum/ui/fragments/directions/DirectionsFragment.kt
+++ app/src/main/java/com/example/pabulum/ui/fragments/directions/DirectionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.pabulum.R
+import com.example.pabulum.models.Result
+import com.example.pabulum.util.Constants
+import kotlinx.android.synthetic.main.fragment_directions.view.*
 
 class DirectionsFragment : Fragment() {
 
@@ -14,7 +18,17 @@ class DirectionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_directions, container, false)
+        val view = inflater.inflate(R.layout.fragment_directions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT)
+
+        view.instructions_source_view.webViewClient = object  : WebViewClient() {}
+        val webUrl: String = myBundle!!.sourceUrl
+        view.instructions_source_view.loadUrl(webUrl)
+
+
+        return view
     }
 
 }
